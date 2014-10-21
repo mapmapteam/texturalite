999e17091b2cf74ecd14db55c2ff7091df50a596
Alexandre Quessy <alexandre@quessy.net>
2014-10-21 15:05:45 -0400

    motor on/off every 50 ms

--- prototypes/serialprint.py
+++ prototypes/serialprint.py
@@ -9,11 +9,25 @@ Dependencies: python-twisted python-serial
 import logging
 import sys
 import serial
+import time
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
 from twisted.internet import task
 from twisted.internet.serialport import SerialPort
 from twisted.python import usage
+
+class Timer(object):
+    """
+    Returns elapsed time.
+    """
+    def __init__(self):
+        self._started = time.time()
+
+    def elapsed(self):
+        return time.time() - self._started
+
+    def reset(self):
+        self._started = time.time()
 
 
 class CliOptions(usage.Options):
@@ -22,7 +36,7 @@ class CliOptions(usage.Options):
     """
     optParameters = [
         ['baudrate', 'b', 115200, 'Serial baudrate'],
-        ['port', 'p', '/dev/ttyACM0', 'Serial port to use'],]
+        ['port', 'p', '/dev/ttyUSB0', 'Serial port to use'],]
 
 
 class Echo(LineReceiver):
@@ -37,7 +51,7 @@ class Echo(LineReceiver):
             self._app.set_hand_connected(True)
         elif data == "n":
             self._app.set_hand_connected(False)
-        print(data)
+        print("Received %s" % (data))
 
     def lineReceived(self, line):
         try:
@@ -60,68 +74,87 @@ class Application(object):
         """
         Constructor.
         """
+        self._serial = None
         self._arduino_connected = False
+        self._timer = Timer()
         # Stores the state of the capsule.
         self._capsule = {
             "hands": False,
             "motor": False,
             "fan": False,
             }
-        logging.debug('About to open port %s' % serial_port)
         try:
+            #logging.debug('About to open port %s' % serial_port)
+            print('About to open port %s' % serial_port)
             self._serial = SerialPort(Echo(self), serial_port, reactor, baudrate=baudrate)
+            print('Success opening port %s' % serial_port)
         except serial.serialutil.SerialException, e:
+            print('Failed opening port %s' % serial_port)
             print(e)
             sys.exit(1)
+
+        print("start task")
         self._looping_call = task.LoopingCall(self._looping_call_cb)
-        self._looping_call.start(50, now=False)
+        self._looping_call.start(0.05, now=False)
 
     def _looping_call_cb(self):
         """
         Called at a regular interval.
         """
+        #print("_looping_call_cb")
         if not self._arduino_connected:
+            print("Arduino is not connected")
             return
         # if the arduino is connected:
 
-        # toggle motor
-        self._capsule["motor"] = not self._capsule["motor"]
-        self.set_motor(self._capsule["motor"])
+        if self._timer.elapsed() >= 1.0:
+            # toggle motor
+            self._capsule["motor"] = not self._capsule["motor"]
+            self.set_motor(self._capsule["motor"])
 
-        # toggle fan
-        self._capsule["fan"] = not self._capsule["fan"]
-        self.set_fan(self._capsule["fan"])
+            # toggle fan
+            self._capsule["fan"] = not self._capsule["fan"]
+            self.set_fan(self._capsule["fan"])
 
-        print("%s" % (self._capsule))
+            print("capsule: %s" % (self._capsule))
+            self._timer.reset()
 
 
     def set_hand_connected(self, is_connected):
         self._capsule["hands"] = is_connected
         if is_connected:
-            print("yes")
+            print("hand yes")
         else:
-            print("no")
+            print("hand no")
 
     def connection_made_cb(self):
         self._arduino_connected = True
+
+        # if self._serial is not None:
+        #     self.set_motor(True)
 
     def set_motor(self, is_enabled):
         """
         Turn on or off the motor.
         """
         if is_enabled:
-            self._serial.sendLine("M")
+            self.send_to_arduino("M")
         else:
-            self._serial.sendLine("m")
+            self.send_to_arduino("m")
 
     def set_fan(self, is_enabled):
         """
         Turn on or off the fan.
         """
         if is_enabled:
-            self._serial.sendLine("F")
+            self.send_to_arduino("F")
         else:
-            self._serial.sendLine("f")
+            self.send_to_arduino("f")
+
+    def send_to_arduino(self, txt):
+        #data = "%s\n" % (txt)
+        print("send %s" % (txt))
+        self._serial.protocol.sendLine(txt) # transport.write(data)
 
 
 def run():
