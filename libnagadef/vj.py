459c6dee70c62ef40ea7f7369c450e94429219d0
Alexandre Quessy <alexandre@quessy.net>
2014-10-22 15:21:04 -0400

    split osc from vj

--- libnagadef/vj.py
+++ libnagadef/vj.py
@@ -23,13 +23,10 @@
 """
 VJ conductor
 """
-import os
-import sys
 from twisted.internet import reactor
 from twisted.internet import task
-from txosc import async
-from txosc import osc
 from libnagadef import urn
+from libnagadef import mapmaposc
 
 class VeeJay(object):
     """
@@ -44,11 +41,7 @@ class VeeJay(object):
         self._current_cue_index = -1 # Initial non-existing cue
         self._urn = urn.UrnRandom()
         self._init_urn()
-        self._osc_port = 12345
-        self._osc_host = "localhost"
-        self._client_proto = None
-        self._client_port = None
-        self._init_osc()
+        self._mapmaposc = mapmaposc.MapMapOsc(12345, "localhost")
 
     def _init_urn(self):
         num = len(self.get_cues())
@@ -65,30 +58,6 @@ class VeeJay(object):
         video_cue = self.get_cues()[index]
         reactor.callLater(video_cue.duration, self.play_next)
 
-        print("play %s" % (video_cue.uri))
-        message = self._create_play_message(video_cue.uri)
-        self._send_osc(message)
-
-    def _init_osc(self, port=12345, host="localhost"):
-        self._client_proto = async.DatagramClientProtocol()
-        self._client_port = reactor.listenUDP(0, self._client_proto)
-
-    def _create_play_message(self, uri):
-        path = "/mapmap/paint/media/load"
-        mapping_index = 0
-        message = osc.Message(path)
-        message.add(mapping_index)
-        message.add(str(uri))
-        return message
-
-    def _send_osc(self, message):
-        """
-        Sends a message using UDP and stops the Reactor
-        @param message: OSC message
-        @type message: L{txosc.osc.Message}
-        @type port: C{int}
-        @type host: C{str}
-        """
-        self._client_proto.send(message, (self._osc_host, self._osc_port))
-        #print("Sent %s to %s:%d" % (message, self._osc_host, self._osc_port))
+        mapping_id = 0
+        self._mapmaposc.play(mapping_id, video_cue.uri)
 
